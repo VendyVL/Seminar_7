0c8ddd51d4d6e55cb75b12ddf6631a39e3e7b7f8
«VendyVL» <vedmansk@yandex.ru>
2022-06-04 16:50:15 +0300

    Задача 52

--- Task_52/Program52.cs
+++ Task_52/Program52.cs
@@ -1,9 +1,38 @@
-﻿// Задача 52: Задайте двумерный массив из целых чисел.
-// Найдите среднее арифметическое элементов в каждом
-// столбце.
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого
 // столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] array52 = new int[,]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+
+double[] AverageArrayColumns(int[,] array)
+{
+    int length = array.GetLength(1);
+    double[] averageColumns = new double[length];
+    double columnLength = array.GetLength(0);
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double a = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            a = a + array[i, j];
+        }
+        double average = a / columnLength;
+        averageColumns[j] = Math.Round(average, 2);
+    }
+    return averageColumns;
+}
+
+double[] averageCol = AverageArrayColumns(array52);
+for (int i = 0; i < averageCol.Length; i++)
+{
+    Console.Write(averageCol[i] + "   ");
+}
